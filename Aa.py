70129ed6a8a734bdf2c44f7d8ee3b7bc74a30d6a
dsv-lt <mirskvad@gmail.com>
2026-02-20 10:50:51 +0500

    Rename bisection to bisection_table and update logic

--- Aa.py
+++ Aa.py
@@ -1,43 +1,49 @@
 import math
 
-# Универсальный метод дихотомии
-def bisection(f, a, b, eps=0.0001):
+def bisection_table(f, a, b, eps=0.0001):
     if f(a) * f(b) > 0:
-        raise ValueError("На данном интервале нет смены знака!")
+        raise ValueError("На интервале нет смены знака")
 
-    iteration = 0
-    print(f"{'№':<3} {'a':<12} {'b':<12} {'c':<12} {'f(c)':<15}")
+    k = 0
+    print(f"{'k':<3} {'a':<12} {'b':<12} {'c':<12} "
+          f"{'f(a)':<12} {'f(b)':<12} {'f(c)':<12} {'|b-a|':<12}")
 
-    while abs(b - a) > eps:
-        c = (a + b) / 2
-        fc = f(c)
+    while True:
+        c = round((a + b) / 2, 6)
+        fa = round(f(a), 6)
+        fb = round(f(b), 6)
+        fc = round(f(c), 6)
+        diff = round(abs(b - a), 6)
 
-        iteration += 1
-        print(f"{iteration:<3} {a:<12.6f} {b:<12.6f} {c:<12.6f} {fc:<15.8f}")
+        print(f"{k:<3} {a:<12.6f} {b:<12.6f} {c:<12.6f} "
+              f"{fa:<12} {fb:<12} {fc:<12} {diff:<12}")
 
-        if f(a) * fc < 0:
+        if diff < eps:
+            break
+
+        if fa * fc < 0:
             b = c
         else:
             a = c
 
-    return (a + b) / 2
+        k += 1
+
+    return round((a + b) / 2, 6)
 
 
-# ===============================
+# ==================================
 # Уравнение 1: x^2 + 4sin(x) = 0
-# ===============================
+# ==================================
 
 def f1(x):
     return x**2 + 4 * math.sin(x)
 
-print("\nУравнение 1: x^2 + 4sin(x) = 0")
+print("\nУравнение 1")
 
-# Корень на [-2, -1]
-root1 = bisection(f1, -2, -1)
-print("\nКорень 1 ≈", round(root1, 6))
+root1 = bisection_table(f1, -2, -1)
+print("\nОтвет:", root1)
 
-# Корень x = 0 (точный)
-print("Корень 2 = 0")
+print("Второй корень: 0.000000")
 
 
 # ==========================================
@@ -47,7 +53,7 @@ print("Корень 2 = 0")
 def f2(x):
     return x**3 + 0.2*x**2 + 0.5*x + 0.8
 
-print("\nУравнение 2: x^3 + 0.2x^2 + 0.5x + 0.8 = 0")
+print("\nУравнение 2")
 
-root2 = bisection(f2, -1, 0)
-print("\nКорень ≈", round(root2, 6))
+root2 = bisection_table(f2, -1, 0)
+print("\nОтвет:", root2)
